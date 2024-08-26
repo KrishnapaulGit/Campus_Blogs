ee69130c2f9c942c725775d8d0013e8905cee0f5
Krishna Paul <krishhmail01@gmail.com>
2024-08-26 20:45:24 +0530

    Creating Navbar using FlowBite React

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,13 @@ import SignIn from './pages/Signin'
 import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
+import Header from './components/Header'
+import Events from './pages/Events'
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
@@ -17,6 +20,7 @@ export default function App() {
         <Route path='/sign-up' element={<SignUp/>}/>
         <Route path='/dashboard' element={<Dashboard/>}/>
         <Route path='/projects' element={<Projects/>}/>
+        <Route path='/events' element={<Events/>}/>
       </Routes>
     </BrowserRouter>
   )
